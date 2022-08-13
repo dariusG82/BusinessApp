e97e35f10eb39f7408feb410f7ef11bbe5149efc
dariusG82 <darius.govedas@gmail.com>
2022-08-13 16:15:05 +0300

    001 - Global - Initial commit

---
+++ src/main/java/eu/dariusgovedas/businessapp/BusinessAppApplication.java
@@ -0,0 +1,13 @@
+package eu.dariusgovedas.businessapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessAppApplication.class, args);
+    }
+
+}
