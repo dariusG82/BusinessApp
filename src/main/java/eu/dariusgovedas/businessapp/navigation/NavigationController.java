2a57e15c7f7edfad5686446f1a539c00244e0c9f
dariusG82 <darius.govedas@gmail.com>
2022-08-28 14:27:10 +0300

    005 - Back-End - Created navigation controller

---
+++ src/main/java/eu/dariusgovedas/businessapp/navigation/NavigationController.java
@@ -0,0 +1,16 @@
+package eu.dariusgovedas.businessapp.navigation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class NavigationController {
+
+    @GetMapping("/home")
+    public String openStartPage(){
+
+        return "startPage";
+    }
+}
